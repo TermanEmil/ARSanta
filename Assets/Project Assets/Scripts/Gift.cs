fa7ca66327ed17a6f33db8bd8be6007cfe41c7af
Daniel <rusnak.dan@mail.ru>
2018-12-23 00:46:52 +0200

    Gift  UI update

--- Assets/Project Assets/Scripts/Gift.cs
+++ Assets/Project Assets/Scripts/Gift.cs
@@ -8,6 +8,12 @@ public class Gift : MonoBehaviour
     public TextMeshPro text;
 
     private bool isReady = false;
+    private Transform lastParent;
+
+    private void Start()
+    {
+        lastParent = transform.parent;
+    }
 
     public void Unpack (Treasure newTreasure) {
         text.text = newTreasure.msg;
@@ -16,20 +22,22 @@ public class Gift : MonoBehaviour
     private void Update()
     {
         text.transform.LookAt(Camera.main.transform.position);
-
-        //transform.LookAt(Camera.main.transform.position);
     }
 
     public void Open()
     {
         if (isReady)
+        {
             GetComponent<Animator>().SetTrigger("open");
+            StartCoroutine(FinishPreview());
+        }
     }
 
     public void Preview ()
     {
-        Transform prevPos = GameObject.Find("gif_recieve_pos").transform;
+        // delete from server
 
+        Transform prevPos = GameObject.Find("gif_recieve_pos").transform;
 
         foreach (var component in Camera.main.GetComponents<Component>())
         {
@@ -40,10 +48,24 @@ public class Gift : MonoBehaviour
         transform.SetParent(prevPos);
         transform.localRotation = Quaternion.identity;
 
-        //Camera.main.gameObject.GetComponent<VuforiaMonoBehaviour>().enabled = false;
-
         isReady = true;
 
         FindObjectOfType<Vuforia.VuforiaBehaviour>().enabled = false;
+    }
+
+    IEnumerator FinishPreview ()
+    {
+        yield return new WaitForSeconds(4);
+
+        FindObjectOfType<Vuforia.VuforiaBehaviour>().enabled = true;
+
+        transform.SetParent(lastParent);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+        GetComponent<Animator>().SetTrigger("idle");
+
+        isReady = false;
+
+        //gameObject.SetActive(false);
     }
 }
