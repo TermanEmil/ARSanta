24862e492aafcb1ad970775c70339a65f5672fe9
Daniel <rusnak.dan@mail.ru>
2018-12-22 18:34:03 +0200

    Uodate 2

---
+++ Assets/Project Assets/Scripts/GiftManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiftManager : MonoBehaviour
+{
+    public GameObject giftPrefab1;
+
+    public Transform pos1;
+
+    public void CreateGift () {
+        Instantiate (giftPrefab1, pos1.position, Quaternion.identity);
+    }
+}
