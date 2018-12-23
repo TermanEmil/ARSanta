59f5f6a615c7b5f7f36f1ee6ba5f7b8fe6e78592
Daniel <rusnak.dan@mail.ru>
2018-12-23 09:26:08 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Project Assets/Scripts/MenuDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MenuDisplay : MonoBehaviour
+{
+    public TextMeshProUGUI orangesText;
+    public TextMeshProUGUI reindeersText;
+    public TextMeshProUGUI bombsText;
+
+    private void Start()
+    {
+        orangesText.text = PlayerPrefs.GetInt("oranges", 0).ToString();
+        reindeersText.text = PlayerPrefs.GetInt("reindeers", 0).ToString();
+        bombsText.text = PlayerPrefs.GetInt("bombs", 0).ToString();
+    }
+
+    public void Play ()
+    {
+        SceneManager.LoadScene("Main2");
+    }
+}
