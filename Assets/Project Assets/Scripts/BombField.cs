912e5bd7585debe05eef39295d693b936f1b6388
Daniel <rusnak.dan@mail.ru>
2018-12-23 07:12:28 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Project Assets/Scripts/BombField.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombField : MonoBehaviour
+{
+    public Transform bombObj;
+    public float startProgress = 0.4f;
+    public float minSize = 1;
+    public float maxSize = 5;
+    public float tapSpeed = 0.15f;
+    public float regenSpeed = 0.11f;
+    public float progress = 0;
+
+    private Treasure treasure;
+    private Gift gift;
+
+    public GameObject explosion;
+
+    public static BombField instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void Init(Gift gift, Treasure treasure)
+    {
+        this.treasure = treasure;
+        this.gift = gift;
+        progress = startProgress;
+
+        print("Init!");
+    }
+
+    private void Update()
+    {
+        if (treasure != null)
+        {
+            if (Input.GetMouseButtonDown(0))
+                progress -= tapSpeed;
+
+            progress += regenSpeed * Time.deltaTime;
+
+            if (progress <= 0)
+            {
+                Win();
+            }
+            else if (progress >= 1)
+            {
+                Loose();
+            }
+            else
+            {
+                bombObj.localScale = Vector3.one * (minSize + (maxSize - minSize) * progress);
+            }
+        }
+        else
+            bombObj.localScale = Vector3.zero;
+    }
+
+    private void Win()
+    {
+        GamePreferences.instance.oranges += treasure.oranges;
+        GamePreferences.instance.reindeers += treasure.reindeers;
+        GamePreferences.instance.SaveData();
+
+        End();
+    }
+
+    private void Loose()
+    {
+        GamePreferences.instance.oranges -= treasure.oranges;
+        GamePreferences.instance.reindeers -= treasure.reindeers;
+
+        if (GamePreferences.instance.oranges < 0)
+            GamePreferences.instance.oranges = 0;
+        if (GamePreferences.instance.reindeers < 0)
+            GamePreferences.instance.reindeers = 0;
+
+        GamePreferences.instance.SaveData();
+
+        var exp = Instantiate(explosion, bombObj.transform.position, Quaternion.identity) as GameObject;
+        Destroy(exp, 2);
+
+        bombObj.transform.localScale = Vector3.zero;
+
+        Invoke("End", 2);
+        treasure = null;
+    }
+
+    private void End()
+    {
+        treasure = null;
+        FindObjectOfType<Vuforia.VuforiaBehaviour>().enabled = true;
+        Destroy(gift.gameObject);
+    }
+}
---
+++ Assets/Project Assets/Scripts/GamePreferences.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GamePreferences : MonoBehaviour
+{
+    public static GamePreferences instance;
+
+    public int oranges;
+    public int reindeers;
+    public int bombs;
+
+    public int orangesInGift;
+    public int reindeersInGift;
+    public int bombsInGift;
+
+    public TextMeshProUGUI orangesText;
+    public TextMeshProUGUI reindeersText;
+    public TextMeshProUGUI bombsText;
+
+    public TextMeshProUGUI orangesTextInGift;
+    public TextMeshProUGUI reindeersTextInGift;
+    public TextMeshProUGUI bombsTextInGift;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        oranges = PlayerPrefs.GetInt("oranges", 0);
+        reindeers = PlayerPrefs.GetInt("reindeers", 0);
+        bombs = PlayerPrefs.GetInt("bombs", 0);
+    }
+
+    private void Update()
+    {
+        orangesTextInGift.text = orangesInGift.ToString();
+        reindeersTextInGift.text = reindeersInGift.ToString();
+        bombsTextInGift.text = bombsInGift.ToString();
+
+        orangesText.text = oranges.ToString();
+        reindeersText.text = reindeers.ToString();
+        bombsText.text = bombs.ToString();
+    }
+
+    public void AddOrange ()
+    {
+        if (oranges <= 0)
+            return;
+
+        oranges--;
+        orangesInGift++;
+    }
+
+    public void AddReideers()
+    {
+        if (reindeers <= 0)
+            return;
+
+        reindeers--;
+        reindeersInGift++;
+    }
+
+    public void AddBombs()
+    {
+        if (bombsInGift > 0 || bombs <= 0)
+            return;
+
+        bombs--;
+        bombsInGift++;
+    }
+
+    public void SaveData ()
+    {
+        PlayerPrefs.SetInt("oranges", oranges);
+        PlayerPrefs.SetInt("reindeers", reindeers);
+        PlayerPrefs.SetInt("bombs", bombs);
+
+        orangesInGift = 0;
+        reindeersInGift = 0;
+        bombsInGift = 0;
+    }
+}
